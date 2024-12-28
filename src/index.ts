b030ee5f0211d18f2dd3cd669366f87ed4617214
Yash Jha <29158046+amitjha329@users.noreply.github.com>
2024-12-28 10:52:19 +0530

    Removed COnsole Logs

--- src/index.ts
+++ src/index.ts
@@ -9,38 +9,10 @@ dotenv.config();
 const app = express();
 const rootDir: string = process.env.ROOT_DIR || __dirname;
 
-// const compressImage = async (imagePath: string): Promise<void> => {
-//   const outputDir = path.dirname(imagePath);
-//   const outputFilePath = path.join(outputDir, `compressed-${path.basename(imagePath)}`);
-//   await sharp(imagePath)
-//     .jpeg({ quality: 85 })
-//     .toFile(outputFilePath);
-// };
-
-// app.post('/compress_images', async (req: Request, res: Response) => {
-//   const walkDir = async (dir: string): Promise<void> => {
-//     const files = await fs.promises.readdir(dir);
-//     for (const file of files) {
-//       const filePath = path.join(dir, file);
-//       const stat = await fs.promises.stat(filePath);
-//       if (stat.isDirectory()) {
-//         await walkDir(filePath);
-//       } else if (file.toLowerCase().endsWith('.png') || file.toLowerCase().endsWith('.jpg') || file.toLowerCase().endsWith('.jpeg')) {
-//         await compressImage(filePath);
-//       }
-//     }
-//   };
-
-//   await walkDir(rootDir);
-//   res.send('Images compressed successfully');
-// });
-
 app.get('/images/*', (req: Request, res: Response) => {
   const options = {
     root: rootDir,
   };
-  console.log("PARAMS================<>>",req.params[0])
-  console.log("PARAMS================<>>",options)
   res.sendFile(req.params[0], options);
 });
 
